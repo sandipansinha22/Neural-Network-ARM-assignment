3e2d49e551c58a9cb86ef8c86ff2dbbff7ab07be
sandipansinha22 <53702961+sandipansinha22@users.noreply.github.com>
2020-11-08 22:44:32 +0530

    Add files via upload

---
+++ printMsg.c
@@ -0,0 +1,113 @@
+#include "stm32f4xx.h"
+#include <string.h>
+void printMsg(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   while(*ptr != '\0'){
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+}
+void printfMsg(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   while(*ptr != '\0'){
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+}
+void printMsg2p(const int a, const int b)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", b);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+}
+
+void printMsg4p(const int a, const int b, const int c, const int d, const int e)
+{
+	 char Msg[100];
+	 char *ptr;
+	 // Printing the message
+	sprintf(Msg,"\n x1:  ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 //Printing the first parameter
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 // Printing the message
+	 sprintf(Msg,"\n x2: ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", b);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 // Printing the message
+	 sprintf(Msg,"\n x3: ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", c);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 // Printing the message
+	 sprintf(Msg,"\nOutput of Sigmoid fn: ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", d);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+	 }
+
+	 
+}
+
